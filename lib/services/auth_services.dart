f27b2b752167cb6f18d49e745c9fbb573b4186d3
Dwi Randy Herdinanto <dwi.randy@amarbank.co.id>
2020-09-10 20:19:13 +0700

    feat(chore): integrate ios/android to firebase

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,12 +1,12 @@
 part of 'services.dart';
 
 class AuthServices {
-  static FirebaseAuth _auth = FirebaseAuth.instance;
+  static FirebaseAuth auth = FirebaseAuth.instance;
 
   static Future<ServiceResult<User>> signUp(String email, String password,
       String name, List<String> selectedGenres, String selectedLanguage) async {
     try {
-      AuthResult result = await _auth.createUserWithEmailAndPassword(
+      AuthResult result = await auth.createUserWithEmailAndPassword(
           email: email, password: password);
 
       User user = result.user.convertToUser(
--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -1,11 +1,11 @@
 part of 'services.dart';
 
 class UserServices {
-  static CollectionReference _userCollection =
+  static CollectionReference userCollection =
       Firestore.instance.collection("users");
 
-  static Future<Void> updateUser(User user) async {
-    _userCollection.document(user.id).setData({
+  static Future updateUser(User user) async {
+    userCollection.document(user.id).setData({
       "email": user.email,
       "name": user.name,
       "balance": user.balance,
