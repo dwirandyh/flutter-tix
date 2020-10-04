4da45610e9e9fa837a7a3bc3bef956182b9c582b
Dwi Randy Herdinanto <dwi.randy@amarbank.co.id>
2020-10-04 19:50:53 +0700

    feat(extension): date extension & date card widget

--- lib/extensions/extensions.dart
+++ lib/extensions/extensions.dart
@@ -3,3 +3,4 @@ import 'package:flutter_tix/models/models.dart';
 import 'package:flutter_tix/services/services.dart';
 
 part 'firebase_user_extension.dart';
+part 'date_time_extension.dart';
--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_tix/models/models.dart';
 import 'package:flutter_tix/shared/shared.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:flutter_tix/extensions/extensions.dart';
 
 part 'selectable_box.dart';
 part 'navigation_bar.dart';
@@ -11,3 +12,4 @@ part 'browse_button.dart';
 part 'coming_soon_card.dart';
 part 'promo_card.dart';
 part 'credit_card.dart';
+part 'date_card.dart';
